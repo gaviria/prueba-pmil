29c5b7c523bdef6166d5d399e5768103f47de14f
Francisco Gaviria Reyes <fjgaviria@i4digital.com>
2024-04-13 02:04:25 -0500

    creando rutas de login, guardado y listado

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,16 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import UsersController from '#controllers/users_controller'
+import SessionController from '#controllers/session_controller'
+import { middleware } from './kernel.js'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router.post('/api/login', [SessionController, 'login'])
+router.get('/api/user', [UsersController, 'index']).use(middleware.auth())
+router.post('/api/user', [UsersController, 'store'])
