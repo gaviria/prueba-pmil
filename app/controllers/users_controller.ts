d69d1af3a5ebe7c324c0d77ec4e58774bbce3087
Francisco Gaviria Reyes <fjgaviria@i4digital.com>
2024-04-13 23:05:04 -0500

    modificando respuestas en controlador

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -4,52 +4,52 @@ import { HttpContext } from '@adonisjs/core/http'
 import { IUser } from '../interfaces/IUser.js'
 import User from '#models/user'
 import { USER_MESSAGES } from '../constants/messages.js'
+import { createUserValidator, updateUserValidator } from '#validators/user'
 
 export default class UsersController {
-  async index({}: HttpContext) {
-    return User.all()
+  async index({ response }: HttpContext) {
+    const users: IUser[] = await User.all()
+    return response.send(users)
   }
 
-  async store({ request }: HttpContext) {
-    const data: IUser = request.only([
-      'firstName',
-      'lastName',
-      'date_birth',
-      'address',
-      'mobile_phone',
-      'email',
-      'password',
-    ])
+  async store({ request, response }: HttpContext) {
+    const data: IUser = await createUserValidator.validate(
+      request.only([
+        'firstName',
+        'lastName',
+        'date_birth',
+        'address',
+        'mobile_phone',
+        'email',
+        'password',
+      ])
+    )
 
     const user: IUser = await User.create(data)
 
-    return user
+    return response.send(user)
   }
 
-  async show({ params }: HttpContext) {
-    return User.findOrFail(params.id)
+  async show({ params, response }: HttpContext) {
+    const user: IUser = await User.findOrFail(params.id)
+    return response.send(user)
   }
 
-  async update({ params, request }: HttpContext) {
+  async update({ params, request, response }: HttpContext) {
     const user = await User.findOrFail(params.id)
-    const data: IUser = request.only([
-      'firstName',
-      'lastName',
-      'date_birth',
-      'address',
-      'mobile_phone',
-      'email',
-      'password',
-    ])
+    const data: IUser = await request.validateUsing(updateUserValidator, {
+      meta: { userId: params.id },
+    })
+
     user.merge(data)
     await user.save()
 
-    return user
+    return response.send(user)
   }
 
-  async destroy({ params }: HttpContext) {
+  async destroy({ params, response }: HttpContext) {
     const user = await User.findOrFail(params.id)
     await user.delete()
-    return { message: USER_MESSAGES.DELETE_SUCCESS, user: user }
+    return response.send({ message: USER_MESSAGES.DELETE_SUCCESS, user: user })
   }
 }
