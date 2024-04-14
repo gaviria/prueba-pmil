077fd73d8703f96974f8714173ca709f410daf71
Francisco Gaviria Reyes <fjgaviria@i4digital.com>
2024-04-13 23:04:31 -0500

    cambio de nombre de variable y response

--- app/controllers/session_controller.ts
+++ app/controllers/session_controller.ts
@@ -1,18 +1,17 @@
-// import type { HttpContext } from '@adonisjs/core/http'
-
 import User from '#models/user'
 import { HttpContext } from '@adonisjs/core/http'
 
 export default class SessionController {
-  async login({ request, auth }: HttpContext) {
+  async login({ request }: HttpContext) {
     const { email, password } = request.only(['email', 'password'])
-    const user = await User.verifyCredentials(email, password)
-    const token = await User.accessTokens.create(user)
+    const userValidated = await User.verifyCredentials(email, password)
+    const token = await User.accessTokens.create(userValidated)
     //console.log(await auth.authenticate()) necesita de token
     //auth.isAuthenticated
     return {
-      user,
-      token: { access_token: token.value!.release(), token_type: 'bearer' },
+      user: userValidated,
+      access_token: token.value!.release(),
+      token_type: 'bearer',
     }
   }
 }
