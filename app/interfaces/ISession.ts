7fa72e656fa05caf9c4155a505564131509a83ce
Francisco Gaviria Reyes <fjgaviria@i4digital.com>
2024-04-16 01:06:23 -0500

    Create ISession.ts

---
+++ app/interfaces/ISession.ts
@@ -0,0 +1,6 @@
+/* eslint-disable unicorn/filename-case */
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export interface ILoginData {
+  mobile_phone: string
+  password: string
+}
